61639380778d46acdfadc832e7ec8fcac45a1c8f
Tanner Seramur <seramurt@gmail.com>
2018-09-12 17:57:03 -0700

    lab 3'
    ls

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Quiz
+var score = 0;
 
 alert("Welcome to my page! Here is a quick quiz to see how much you know about me");
 
@@ -9,6 +10,7 @@ console.log(answer.toLowerCase());
 
 if(answer.toLowerCase() === "tanner" ){
     alert("Correct!");
+    score = score +1; 
 }else{
     alert("Incorrect :(");
 }
@@ -18,6 +20,7 @@ console.log(answer.toLowerCase());
 
 if((answer.toLowerCase() === "yes") || (answer.toLowerCase() === "y") ){
     alert("Correct!");
+    score = score +1;
 }else if((answer.toLowerCase() === "no") || (answer.toLowerCase() === "n")){
     alert("Incorrect :(");
 }else{
@@ -29,6 +32,7 @@ console.log(answer.toLowerCase());
 
 if((answer.toLowerCase() === "no") || (answer.toLowerCase() === "n") ){
     alert("Correct!");
+    score = score +1;
 }else if((answer.toLowerCase() === "yes") || (answer.toLowerCase() === "y")){
     alert("Incorrect :(");
 }else{
@@ -40,6 +44,7 @@ console.log(answer.toLowerCase());
 
 if((answer.toLowerCase() === "yes") || (answer.toLowerCase() === "y") ){
     alert("Correct!");
+    score = score +1;
 }else if((answer.toLowerCase() === "no") || (answer.toLowerCase() === "n")){
     alert("Incorrect :(");
 }else{
@@ -51,11 +56,76 @@ console.log(answer.toLowerCase());
 
 if((answer.toLowerCase() === "no") || (answer.toLowerCase() === "n") ){
     alert("Correct!");
+    score = score +1;
 }else if((answer.toLowerCase() === "yes") || (answer.toLowerCase() === "y")){
     alert("Incorrect :(");
 }else{
     alert("Incorrect :(");
 }
+
+// lab 3 work starts here
+// for loop that gives users 4 guesses
+
+for(var i = 0; i < 4; i++){
+    var numGuess = prompt("Guess my favorite number?")
+  
+    if(numGuess < 10){
+      alert("Too low! Guess higher!");
+    }else if(numGuess > 10){
+      alert("Too high! Guess lower!");
+    }else if(numGuess === 10){
+      alert("Nice! You got it!");
+      score = score +1;
+      break;
+    }else{
+      alert("wrong");
+    }
+  }
+
+ 
+alert("During my life I have only lived in 2 states can you guess them");
+var guessArray = [];
+
+for(var i = 0;i <= 6; i++ ){
+  var stateArray1 = ['washington','montana'];
+  var stateArray2 = ['montana','washington'];
+
+  
+  var stateGuess = prompt("Guess a state:");
+
+  switch(stateGuess.toLowerCase()){
+    case 'washington':
+      guessArray.push('washington');
+      alert("Correct! Washington is one of them!");
+      break;
+
+    case 'montana':
+      guessArray.push('montana');
+      alert("Correct! Montana is one of them!");
+      break;
+
+    default:
+    alert("Nice try but that is not one. I have lived in too states, here is a list of options to guess");
+    alert("Washington, Oregon, California, Idaho, Montana, Arizona");
+    break;
+  }
+
+  if((guessArray.toString() === stateArray1.toString()) || (guessArray.toString() === stateArray2.toString())){
+    alert("You got them both!")
+    score = score +1; 
+    break;
+  }else{
+
+  }
+  
+}
+
+alert("You have scored "+ score + " out of 7");
+
+
+
+
+
 
 
 // POP-UP STARTS HERE
